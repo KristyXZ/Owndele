32f39d2b4250827b7aac109d9389816dc035b6fc
Atharva <125717395+KristyXZ@users.noreply.github.com>
2025-04-16 19:23:37 +0530

    Update config.py

--- auto_delete_bot/config.py
+++ auto_delete_bot/config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7859184332:AAFPrkZyrli8RJjaGeX_JClFKiJU4owIg4o")
+API_ID = int(os.getenv("API_ID", 29394851))
+API_HASH = os.getenv("API_HASH", "4a97459b3db52a61a35688e9b6b86221")
 SESSION_STRING = os.getenv("SESSION_STRING")
